78d4330f4d40459ab43205baff5f5374ac6f124b
HossainRafi <hossainrafi71@gmail.com>
2022-05-11 12:31:20 +0600

    necessary items installed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import Navbar from "./Pages/Shared/Navbar";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
 
 
 function App() {
   return (
     <div>
-      
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
     </div>
   );
 }
