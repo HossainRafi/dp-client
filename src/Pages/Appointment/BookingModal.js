38a2b403e8608f725037d56ecfd8f985e4ca0c94
HossainRafi <hossainrafi71@gmail.com>
2022-05-16 17:39:03 +0600

    properly added services

--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -4,7 +4,7 @@ import auth from './../../firebase.init';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { toast } from 'react-hot-toast';
 
-const BookingModal = ({ treatment, date, setTreatment }) => {
+const BookingModal = ({ treatment, date, setTreatment,refetch }) => {
   const { _id, name, slots } = treatment;
   const [user, loading, error] = useAuthState(auth);
  const formattedDate = format(date, "PP");
@@ -40,11 +40,10 @@ const booking = {
             `Already have an appointment on ${data.booking?.date} at ${data.booking?.slot}`
           );
         }
+        refetch()
         // For close the modal
         setTreatment(null);
       });
-    
-    // setTreatment(null);
   };
   return (
     <div>
